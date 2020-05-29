b29123009aa1faec07c4571f9e4a9afac810dbfb
amultari <a.multari@solvingteam.it>
2020-05-29 19:41:02 +0200

    terminata implementazione paginazione e query dinamica per ricerca

--- src/main/java/com/example/automobile_rest/service/AutomobileServiceImpl.java
+++ src/main/java/com/example/automobile_rest/service/AutomobileServiceImpl.java
@@ -1,13 +1,18 @@
 package com.example.automobile_rest.service;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.persistence.criteria.Predicate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.example.automobile_rest.model.Automobile;
 import com.example.automobile_rest.repository.AutomobileRepository;
@@ -26,11 +31,36 @@ public class AutomobileServiceImpl implements AutomobileService {
 	@Override
 	public Page<Automobile> searchAndPaginate(Automobile automobileExample, Integer pageNo, Integer pageSize,
 			String sortBy) {
+
+		Specification<Automobile> specificationCriteria = (root, query, cb) -> {
+
+			List<Predicate> predicates = new ArrayList<Predicate>();
+
+			if (!StringUtils.isEmpty(automobileExample.getMarca()))
+				predicates.add(cb.like(root.get("marca"), "%" + automobileExample.getMarca() + "%"));
+
+			if (!StringUtils.isEmpty(automobileExample.getModello()))
+				predicates.add(cb.like(root.get("modello"), "%" + automobileExample.getModello() + "%"));
+
+			if (!StringUtils.isEmpty(automobileExample.getTarga()))
+				predicates.add(cb.like(root.get("targa"), "%" + automobileExample.getTarga() + "%"));
+
+			if (automobileExample.getDataImmatricolazione() != null)
+				predicates.add(cb.greaterThanOrEqualTo(root.get("dataImmatricolazione"),
+						automobileExample.getDataImmatricolazione()));
+
+			if (automobileExample.getInProduzione() != null)
+				if (automobileExample.getInProduzione())
+					predicates.add(cb.isTrue(root.get("inProduzione")));
+				else
+					predicates.add(cb.isFalse(root.get("inProduzione")));
+
+			return cb.and(predicates.toArray(new Predicate[predicates.size()]));
+		};
+
 		Pageable paging = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
 
-		Page<Automobile> pagedResult = automobileRepository.findAll(paging);
-
-		return pagedResult;
+		return automobileRepository.findAll(specificationCriteria, paging);
 	}
 
 	@Override
